cd12a58bfd31a459bcb4a1ae632f832cec327c59
HoziusTeo <hoza.teodor@yahoo.com>
2023-11-05 17:50:14 +0200

    latest version

--- src/main/java/com/sda/app/repository/ItemRepository.java
+++ src/main/java/com/sda/app/repository/ItemRepository.java
@@ -1,2 +1,10 @@
-package com.sda.app.repository;public class ItemRepository {
+package com.sda.app.repository;
+
+import com.sda.app.entity.Item;
+import com.sda.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository <Item,Integer> {
 }
